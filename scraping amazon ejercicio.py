5eccb2f0819a33fe43d579ff13cb6371bd21d330
juanjo <juanjose.mquesada@gmail>
2022-05-23 15:48:44 +0200

    Segunda Version

--- scraping amazon ejercicio.py
+++ scraping amazon ejercicio.py
@@ -21,42 +21,57 @@ boton.click()
 barra_busqueda = browser.find_element(by=By.XPATH, value='//*[@id="twotabsearchtextbox"]').send_keys(busqueda1 + Keys.ENTER)
 time.sleep(1)
 
-try:
-    num_page = browser.find_element(By.XPATH, value="//*[@id='search']/div[1]/div[1]/div/span[3]/div[2]/div[58]/div/div/span/span[4]")
-except NoSuchElementException:
-    num_page = browser.find_element(By.CLASS_NAME, value='a-last').click()
-
-browser.implicitly_wait(3)
-
-url_list = []
-
-for i in range(int(num_page.text)):
-    page_ = i + 1
-    url_list.append(browser.current_url)
-
-    browser.implicitly_wait(4)
-    click_next = browser.find_element(By.CLASS_NAME, value='a-last').click()
-    print("Page " + str(page_) + " grabbed")
-print(url_list)
-
-'''time.sleep(2)
-panel = browser.find_element(by=By.CLASS_NAME, value="s-main-slot s-result-list s-search-results sg-row".replace(" ", "."))
-moviles = panel.find_elements(by=By.XPATH, value="//div[@data-component-type='s-search-result']")
-for movil in moviles:
-    titulo = movil.find_element(by=By.TAG_NAME, value="H2")
-    imagen = movil.find_element(by=By.CLASS_NAME, value="s-image".replace(" ", ".")).get_attribute("src")
+def extraer_url():
     try:
-        precio = movil.find_element(by=By.CLASS_NAME, value="a-price-whole")
-    except:
-        pass
-    link = movil.find_element(by=By.CLASS_NAME, value= "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal".replace(" ",".")).get_attribute("href")
+        num_page = browser.find_element(By.XPATH, value="//*[@id='search']/div[1]/div[1]/div/span[3]/div[2]/div[58]/div/div/span/span[4]")
+    except NoSuchElementException:
+        num_page = browser.find_element(By.CLASS_NAME, value='a-last').click()
+    browser.implicitly_wait(3)
+    url_list = []
+    for i in range(int(num_page.text)):
+        page_ = i + 1
+        url_list.append(browser.current_url)
 
-    #print(titulo.text)
-    #print(imagen)
-    #print(precio)
-    #print(link)'''
+        browser.implicitly_wait(4)
+        click_next = browser.find_element(By.CLASS_NAME, value='a-last').click()
+        print("Page " + str(page_) + " grabbed")
 
+    browser.quit()
+    return url_list
 
+def extraer_informacion():
+    paginas = extraer_url()
+
+    browser = webdriver.Chrome()
+
+    for pagina in paginas:
+
+        time.sleep(2)
+        #boton = browser.find_element(by=By.XPATH, value=('//*[@id="sp-cc-accept"]'))
+        #boton.click()
+        browser.get(pagina)
+        panel = browser.find_element(by=By.CLASS_NAME, value="s-main-slot s-result-list s-search-results sg-row".replace(" ", "."))
+        moviles = panel.find_elements(by=By.XPATH, value="//div[@data-component-type='s-search-result']")
+
+        for movil in moviles:
+
+            titulo = movil.find_element(by=By.TAG_NAME, value="H2")
+            imagen = movil.find_element(by=By.CLASS_NAME, value="s-image".replace(" ", ".")).get_attribute("src")
+            try:
+                precio = movil.find_element(by=By.CLASS_NAME, value="a-price-whole")
+            except:
+                pass
+            link = movil.find_element(by=By.CLASS_NAME, value= "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal".replace(" ",".")).get_attribute("href")
+
+            print(titulo.text)
+            print(imagen)
+            print(precio.text)
+            print(link)
+
+        print('Fin pagina' + pagina)
+        print('--------------------')
+    browser.quit()
+extraer_informacion()
 
 
 
